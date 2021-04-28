55514ba88dcc591316915714967fd401b0caa521
Sandeep Aggarwal <sandeep.aggarwal22@gmail.com>
2021-04-28 10:11:33 +0930

    Added library files

---
+++ projects/users/src/lib/users.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-users',
+  template: ` <p>Welcome to Users module</p> `,
+  styles: [],
+})
+export class UsersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
